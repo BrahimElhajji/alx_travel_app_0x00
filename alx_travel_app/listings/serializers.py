983036f56eeaf644e6ba4240c48d2ab74d488bd9
BrahimElhajji <elhajji080898@gmail.com>
2025-05-31 14:06:10 +0100

    still fixing the serializers file

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Listing, Booking
+from .models import Review
 
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +10,9 @@ class ListingSerializer(serializers.ModelSerializer):
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
         fields = '__all__'
